1707ee7ebff92bb71a3f34886c1ee8f39f5f638a
Pallavi Dwivedi <dwivedi.pallavi9@gmail.com>
2023-10-17 11:22:14 -0500

    initial commit changes

--- App.js
+++ App.js
@@ -10,28 +10,25 @@ console.log(heading);
 //root.render(heading);
 
 /* 
-1st dom structure 
-<div id="parent1">
-    <div id="child">
-        <h1></h1>
-    </div>
-</div>
-
-2nd dom struvture
+Desired DOM structure
 <div id="parent2">
     <div id="child1">
         <h1></h1>
+        <h2></h2>
     </div>
     <div id="child2">
         <h1></h1>
+        <h2></h2>
     </div>
 </div> */
 
-const parent1 = React.createElement('div', {id: 'parent1'}, React.createElement('div', {id: 'child'}, 
-    React.createElement('h1', {}, 'I am the h1')));
-
-const parent = React.createElement('div', {id: 'parent2'}, React.createElement('div', {id: 'child'}, 
-    [React.createElement('h1', {id: 'h1'}, 'I am the h1'), React.createElement('h2', {id: 'h2'}, 'I am the h2')]));
+const parent = React.createElement('div', {id: 'parent'},
+    [React.createElement('div', {id: 'child1'}, 
+        [React.createElement('h1', {}, 'I am the h1 in child1')], 
+        [React.createElement('h2', {}, 'I am h2 in child1')])], 
+    [React.createElement('div', {id: 'child2'}, 
+        [React.createElement('h1', {id: 'h1'}, 'I am the h1 in child2')], 
+        [React.createElement('h2', {id: 'h2'}, 'I am the h2 in child2')])]);
 
 root.render(parent);
 
