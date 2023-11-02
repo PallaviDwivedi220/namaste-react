a283fa90592bcce14f4ef8f1dfb7b2c0e04092e2
Pallavi Dwivedi <dwivedi.pallavi9@gmail.com>
2023-11-02 09:28:03 -0500

    about components

--- App.js
+++ App.js
@@ -2,33 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const heading = React.createElement('h1', {className: 'heading', id: 'xyz'}, 'Hello World from React!');
 
-console.log(heading);
+//React Element
+const Title = () => (<h1 id="heading" className="heading">Hello World from React using JSX!</h1>);
+const num = 899999;
 
-//root.render(heading);
+//React Functional Component
+const HeadingComponent = () => (<div className="container">
+        <Title />
+        <h1>{num + 2 }</h1>
+        <h1 className="heading">Hello World from React using JSX Functional Component!</h1>
+    </div>);
 
-/* Desired DOM structure
-<div id="parent2">
-    <div id="child1">
-        <h1></h1>
-        <h2></h2>
-    </div>
-    <div id="child2">
-        <h1></h1>
-        <h2></h2>
-    </div>
-</div> */
 
-const parent = React.createElement('div', {id: 'parent'},[
-    React.createElement('div', {id: 'child1'}, [
-        React.createElement('h1', {}, 'I am the h1 in child1'),
-        React.createElement('h2', {}, 'I am h2 in child1')
-    ])], 
-    React.createElement('div', {id: 'child2'}, [
-        React.createElement('h1', {id: 'h1'}, 'I am the h1 in child2'),
-        React.createElement('h2', {id: 'h2'}, 'I am the h2 in child2')
-    ]));
-
-root.render(parent);
-
+root.render(<HeadingComponent/>);
